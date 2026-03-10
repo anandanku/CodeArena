989f7083f08102c77706c7802a86ab44ae7de731
anandanku <121048764+anandanku@users.noreply.github.com>
2026-03-11 05:24:53 +0530

    Update problems.js

--- problems.js
+++ problems.js
@@ -1,3 +1,5 @@
+import express from "express";
+const router=express.Router();
 router.get("/problems", async (req, res) => {
 
   try {
@@ -36,7 +38,9 @@ router.get("/problems", async (req, res) => {
 
   }
 
+export default router;
 
 });
 
 export default router;
+
