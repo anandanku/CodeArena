cbb5ca370b14cef827abd64b3c3e3bb469be0a84
anandanku <121048764+anandanku@users.noreply.github.com>
2026-03-11 05:11:29 +0530

    Update redisconnection.js

--- redisconnection.js
+++ redisconnection.js
@@ -1,5 +1,7 @@
-const { createClient } = require("redis");
-require("dotenv").config();
+import { createClient } from "redis";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const redis = createClient({
   url: process.env.REDIS_URL
@@ -23,3 +25,4 @@ redis.on("error", (err) => {
 
 
 export default redis;
+
