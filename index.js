2c8e06b84d45b00d3e4ed6d721c926af560ddd88
anandanku <121048764+anandanku@users.noreply.github.com>
2026-03-11 03:55:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -109,4 +109,7 @@ app.get("/dashboard", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
