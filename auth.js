8e05a6d3f0da4d22bef7318d09b71b7ccdd1e059
anandanku <121048764+anandanku@users.noreply.github.com>
2026-08-23 21:46:55 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -65,6 +65,12 @@ passport.use(
 );
 router.get(
   "/google",
+  (req,res,next)=>{
+    if(req.isAuthenticated && req.isAuthenticated()){
+      return res.redirect("/home")
+    }
+    next()
+  },
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
